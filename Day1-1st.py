6972cf22613893848d43954b903e33f8e235ecbb
Nagesha G <nagesha3103@gmail.com>
2026-07-08 21:31:39 +0530

    Add files via upload

---
+++ Day1-1st.py
@@ -0,0 +1,5 @@
+#code for your first project that is nick name of yours 
+print("Welcome to the Nicknames of yours")
+Tag=input("Enter Your Tag:")
+name=input("Enter Your Hero Name:")
+print(Tag,name)
